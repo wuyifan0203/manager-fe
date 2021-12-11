fd9b765c9fcc291991b582089d2dec67aa4b5010
wuyifan <1208097313@qq.com>
2021-12-12 00:01:01 +0800

    feat: 完成布局，添加stylelint

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,54 @@
+<template>
+  <div class="basic-layout">
+    <div class="nav-side"></div>
+    <div class="content-right">
+      <nav class="nav-top"></nav>
+      <main class="wrapper">
+        <div class="main-page">
+          <router-view></router-view>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
 <script>
-import { ref } from 'vue';
-export default{
+export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name:'Home'
+  name: 'Home',
 };
 </script>
 
-<template>
- <h1>Home</h1>
-</template>
-
-<style scoped>
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.basic-layout {
+  position: relative;
+  .nav-side {
+    position: fixed;
+    width: 200px;
+    height: 100vh;
+    background-color: #001529;
+    color: #fff;
+    overflow-y: auto;
+    transition: width 0.5s;
+  }
+  .content-right {
+    margin-left: 200px;
+    .nav-top {
+      display: flex;
+      height: 50px;
+      line-height: 50px;
+      border-bottom: 1px solid #ddd;
+      justify-content: space-between;
+      padding: 0 20px;
+    }
+    .wrapper {
+      background-color: #eef0f3;
+      padding: 20px;
+      height: calc(100vh - 50px);
+      .main-page {
+        background-color: #fff;
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/Welcom.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'Welcome',
+    setup() {
+        
+    },
+};
+</script>
+<style lang="sass" scoped>
+
+</style>
