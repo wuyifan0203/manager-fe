1210c0cfcd0738938af223042f131a1446fb8bcb
wuyifan <1208097313@qq.com>
2021-12-18 18:27:15 +0800

    完成登录模块

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,11 +1,11 @@
-import storage from "../utils/storage";
+import storage from "../../utils/storage";
 
 const state = {
     userInfo:'' || storage.getItem('userInfo')
 };
 
 const mutations = {
-    SET_UserInfo(state,userInfo){
+    setUserInfo(state,userInfo){
         state.userInfo=userInfo;
         storage.setItem('userInfo',userInfo);
     }
