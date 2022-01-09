8a3f5fbd87fcdd4ae50f0b3258a384e22e8402cd
wuyifan <1208097313@qq.com>
2022-01-10 01:19:27 +0800

    'feat:添加user界面'

---
+++ src/components/ColaTable.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-table :data="data">
+    <el-table-column prop="date" label="Date" width="180" />
+  </el-table>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ColaTable',
+  props:{
+      data:{
+          type:Array,
+          default:()=>[]
+      },
+      config:{
+          type:Object,
+          default:()=>{}
+      }
+  },
+  setup() {},
+});
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/views/User.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="user-manage">
+    <div class="query-form">
+      <el-form :inline="true">
+        <el-form-item>
+          <el-input
+            v-model="formParams.userId"
+            placeholder="please input user id"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="formParams.userName"
+            placeholder="please input user name"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-select
+            v-model="formParams.state"
+            placeholder="please select working state"
+          >
+            <el-option
+              v-for="opt in optionList"
+              :key="opt.key"
+              :value="opt.value"
+              :label="opt.key"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">query</el-button>
+          <el-button>reset</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="base-table">
+      <div class="action">
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column prop="date" label="Date" width="180" />
+          <el-table-column prop="name" label="Name" width="180" />
+          <el-table-column prop="address" label="Address" />
+        </el-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, reactive } from 'vue';
+export default defineComponent({
+  name: 'User',
+  setup() {
+    const optionList = [
+      { key: 'working', value: 0 },
+      { key: 'probation', value: 1 },
+      { key: 'intern', value: 2 },
+      { key: 'departing', value: 3 },
+    ];
+    const formParams = reactive({
+      userId: '',
+      userName: '',
+      state: '',
+    });
+
+    return {
+      formParams,
+      optionList,
+    };
+  },
+});
+</script>
+
+<style lang="sass" scoped></style>
